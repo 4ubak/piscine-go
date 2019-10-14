a025f22210b9d3c5ea79ecfa82389e0d42796895
Temirlan <ib.temirlan@gmail.com>
2019-10-15 00:02:59 +0600

    myfamily added

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl --silent https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq '[.[].connections] | .[0].relatives'
+curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --arg HERO_ID "$HERO_ID" '.[] |select (.id == ($HERO_ID|tonumber)) |.connections.relatives' | sed 's/*//g' 
