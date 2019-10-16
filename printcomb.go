092504e6ff727990eccf4804f4c8f6b20a7670ee
Temirlan <ib.temirlan@gmail.com>
2019-10-16 23:08:59 +0600

    printcomb.go added

--- printcomb.go
+++ printcomb.go
@@ -3,7 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for a := '0'; a <= '6'; a++ {
+	for a := '0'; a <= '7'; a++ {
 		for b := a + 1; b <= '9'; b++ {
 			for c := b + 1; c <= '9'; c++ {
 				if a < b {
@@ -11,8 +11,13 @@ func PrintComb() {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(c)
-						z01.PrintRune(44)
-						z01.PrintRune(32)
+						if a == '7' | b == '8' | c == '9' {
+
+						}else {
+							z01.PrintRune(44)
+							z01.PrintRune(32)
+						}
+
 					}
 				}
 			}
