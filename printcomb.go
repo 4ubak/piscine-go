16d7a5998f73e443f3eb1605a389aca92421b689
Temirlan <ib.temirlan@gmail.com>
2019-10-16 22:52:43 +0600

    printcomb.go added

--- printcomb.go
+++ printcomb.go
@@ -3,10 +3,10 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for startNumber := 0; startNumber <= 999; startNumber++ {
-		var fisrtDigit = startNumber / 100
-		var secondDigit = (startNumber / 10) % 10
-		var thirdDigit = startNumber % 10
+	for startNumber := '0'; startNumber <= '999'; startNumber++ {
+		var fisrtDigit = startNumber / '100'
+		var secondDigit = (startNumber / '10') % '10'
+		var thirdDigit = startNumber % '10'
 		if fisrtDigit < secondDigit {
 			if secondDigit < thirdDigit {
 				z01.PrintRune(startNumber)
