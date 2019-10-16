3e1abe00b5fe795dace3ba50f80dd5f1fb5f6573
Temirlan <ib.temirlan@gmail.com>
2019-10-16 22:06:15 +0600

    isnegative.go added

--- isnegative.go
+++ isnegative.go
@@ -1,14 +1,6 @@
-package main
-
-import piscine ".."
+package piscine
 
 import "github.com/01-edu/z01"
-
-func main() {
-	piscine.IsNegative(1)
-	piscine.IsNegative(0)
-	piscine.IsNegative(-1)
-}
 
 func IsNegative(nb int) {
 	if nb >= 0 {
