15d943cc60d9c95f04e8d06b520091f54e8d98b0
Temirlan <ib.temirlan@gmail.com>
2019-10-16 22:20:12 +0600

    isnegative.go added

--- isnegative.go
+++ isnegative.go
@@ -5,8 +5,9 @@ import "github.com/01-edu/z01"
 func IsNegative(nb int) {
 	if nb >= 0 {
 		z01.PrintRune('F')
+		z01.PrintRune(10)
 	} else {
 		z01.PrintRune('T')
+		z01.PrintRune(10)
 	}
-	z01.PrintRune(10)
 }         
