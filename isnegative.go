4e339b549ea82d1935817e91b6f8b8ecbb1bdff9
Temirlan <ib.temirlan@gmail.com>
2019-10-16 22:04:04 +0600

    isnegative.go added

---
+++ isnegative.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/01-edu/z01"
+import piscine ".."
+
+func main() {
+	piscine.IsNegative(1)
+	piscine.IsNegative(0)
+	piscine.IsNegative(-1)
+}
+
+func IsNegative(nb int) {
+	if nb >= 0 {
+		z01.PrintRune('F')
+	} else {
+		z01.PrintRune('T')
+	}
+}
