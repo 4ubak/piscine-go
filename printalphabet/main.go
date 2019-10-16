5afd686b623482c68c7f65c771a7b6e0800b48ff
Temirlan <ib.temirlan@gmail.com>
2019-10-16 21:47:59 +0600

    main.go added

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for smallAlph := 'a'; smallAlph <= 'z' ; smallAlph++ {
+	for smallAlph := 'a'; smallAlph <= 'z'; smallAlph++ {
 		z01.PrintRune(rune(smallAlph))
 	}
 	z01.PrintRune(10)
