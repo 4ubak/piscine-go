1f28fb34fea6e2b31d7b590eab3ee5798c8da283
Temirlan <ib.temirlan@gmail.com>
2019-10-16 22:30:01 +0600

    added main.go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,15 +1,10 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
 func main() {
 	for smallAlph := 'a'; smallAlph <= 'z'; smallAlph++ {
 		z01.PrintRune(rune(smallAlph))
-	}
-	z01.PrintRune(10)
-
-	for bigAlph := 'A'; bigAlph <= 'Z'; bigAlph++ {
-		z01.PrintRune(rune(bigAlph))
 	}
 	z01.PrintRune(10)
 }
