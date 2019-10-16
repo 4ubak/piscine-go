64285611ae8c34f7d0b6ecb652db593892a6df56
Temirlan <ib.temirlan@gmail.com>
2019-10-16 23:03:08 +0600

    printcomb.go added

--- printcomb.go
+++ printcomb.go
@@ -4,8 +4,8 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for a := '0'; a <= '9'; a++ {
-		for b:= a+1; b <= '9'; b++ {
-			for c:= b+1; c <= '9'; c++{
+		for b := a + 1; b <= '9'; b++ {
+			for c := b + 1; c <= '9'; c++ {
 				if a < b {
 					if b < c {
 						z01.PrintRune(a)
