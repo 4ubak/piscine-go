765d2d0c4254e9a695210cf810b39b622bf3ff6d
Temirlan <ib.temirlan@gmail.com>
2019-10-16 21:56:21 +0600

    main.go added

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func main() {
 	for reversealAlph := 'z'; reversealAlph >= 'a'; reversealAlph-- {
-		z01.PrintRune(rune(reversealAlph))
+		z01.PrintRune(reversealAlph)
 	}
 	z01.PrintRune(10)
 }
