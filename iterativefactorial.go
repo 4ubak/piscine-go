ceae12fb86e8246c8c933f326879ac2041fb955a
4ubak <tima.75.com@gmail.com>
2019-10-21 13:57:10 +0600

    1 - task

---
+++ iterativefactorial.go
@@ -0,0 +1,16 @@
+
+package piscine
+
+func IterativeFactorial(nb int) int {
+	if nb == 0 {
+		return 1
+	}
+	if nb < 0 {
+		return 0
+	}
+	var b = 1
+	for a := nb; a >= 1; a -- {
+		b *= a 
+	} 
+	return b
+}
