4883557a3a377c5d3bb2602532180faa2690e480
4ubak <tima.75.com@gmail.com>
2019-10-21 14:05:33 +0600

    4 - quest, 1 - task

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,15 +1,17 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb == 0 {
-		return 1
+	if nb < 24 {
+		if nb == 0 {
+			return 1
+		}
+		if nb < 0 {
+			return 0
+		}
+		var b = 1
+		for a := nb; a >= 1; a-- {
+			b *= a
+		}
+		return b
 	}
-	if nb < 0 {
-		return 0
-	}
-	var b = 1
-	for a := nb; a >= 1; a-- {
-		b *= a
-	}
-	return b
 }
