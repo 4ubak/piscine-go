f707b84d9874dc5d916da49186fd9d8d5462640c
4ubak <tima.75.com@gmail.com>
2019-10-21 15:39:06 +0600

    4-quest, 7-task

--- isprime.go
+++ isprime.go
@@ -2,7 +2,10 @@ package piscine
 
 func IsPrime(nb int) bool {
 	if nb > 0 {
-		if nb%1 == 0 && nb%nb == 0 && nb%2 == 0 {
+		if nb == 2 {
+			return true
+		}
+		if (nb%1 == 0 || nb%2 == 0) && nb%nb == 0 {
 			return false
 		} else {
 			return true
