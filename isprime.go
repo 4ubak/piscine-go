7d095d886648e84b4c5934d9892ec49edf7ab877
4ubak <tima.75.com@gmail.com>
2019-10-21 16:16:50 +0600

    Update isprime.go

--- isprime.go
+++ isprime.go
@@ -11,6 +11,11 @@ func IsPrime(nb int) bool {
 		if nb%3 == 0 || nb%2 == 0 {
 			return false
 		}
+		for a := 2; a < nb; a++ {
+			if nb%a == 0 {
+				return false
+			}
+		}
 		return true
 	} else {
 		return false
