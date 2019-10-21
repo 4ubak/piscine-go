655a11896c3f3a4d4aa3ae2581901dd2e1e193ef
4ubak <tima.75.com@gmail.com>
2019-10-21 14:13:47 +0600

    4-quest, 3-task

---
+++ iterativepower.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	if power == 0 {
+		return 1
+	} 
+	if power < 0 {
+		return 0
+	} else {
+		return nb * IterativePower(nb, power-1)
+	}
+}
