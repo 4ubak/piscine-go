27a986723cf861b58f97587d5517d20d908641e0
4ubak <tima.75.com@gmail.com>
2019-10-21 14:57:10 +0600

    4-quest, 6-task

--- sqrt.go
+++ sqrt.go
@@ -1,11 +1,15 @@
 package piscine
 
 func Sqrt(nb int) int {
-	result := 1
-	sqrt := 0
-	for a := 1; result <= nb; a++ {
-		result = a * a
-		sqrt++
+	if nb%2 == 0 && nb > 0 {
+		result := 1
+		sqrt := 0
+		for a := 1; result <= nb; a++ {
+			result = a * a
+			sqrt++
+		}
+		return sqrt - 1
+	} else {
+		return 0
 	}
-	return sqrt - 1
 }
