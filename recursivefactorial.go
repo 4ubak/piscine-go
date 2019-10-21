8063fbe42ca4bb8646d82af585b6281fb762eed3
4ubak <tima.75.com@gmail.com>
2019-10-21 14:11:21 +0600

    4-quest, 2-task

---
+++ recursivefactorial.go
@@ -0,0 +1,15 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	if nb < 25 {
+		if nb == 0 {
+			return 1
+		}
+		if nb < 0 {
+			return 0
+		} else {
+			return nb * RecursiveFactorial(nb - 1)
+		}
+	}
+	return 0
+}
