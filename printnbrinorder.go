fd225172923953b8d457e4225867502f570390f7
Temirlan <ib.temirlan@gmail.com>
2019-10-22 19:47:41 +0600

    5-quest, 2-task

---
+++ printnbrinorder.go
@@ -0,0 +1,30 @@
+package piscine
+
+func PrintNbrInOrder(n int) {
+	if n == 0 {
+		fmt.Print(0)
+		return
+	}
+	if n > 0 {
+		var array []int
+		minValue := 0
+		eachValue := 0
+		result := 0
+		for n != 0 {
+			eachValue = n % 10
+			n /= 10
+			if eachValue < minValue {
+				minValue = eachValue
+				array = append([]int{minValue}, array...)
+			} else {
+				minValue = eachValue
+				array = append(array, minValue)
+			}
+		}
+		for i := 0; i < len(array); i++ {
+			result *= 10
+			result = result + array[i]
+		}
+		fmt.Print(result)
+	}
+}
