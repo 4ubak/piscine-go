265af5f2da9e62a17e912277127e2d45e31a3137
Temirlan <ib.temirlan@gmail.com>
2019-10-22 22:47:54 +0600

    5-quest, 19-task

--- join.go
+++ join.go
@@ -4,13 +4,14 @@ func Join(strs []string, sep string) string {
 	result := ""
 	count := 0
 	for i := range strs {
-		count++
+		count = i + 1
 	}
 	for i, str := range strs {
-		if i == count {
+		if i == count-1 {
 			result += str
+		} else {
+			result += str + sep
 		}
-		result += str + sep
 	}
 	return result
 }
