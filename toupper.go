0f36b5c8f2f1ce9b6f4b19ade7dca67c37c195ae
Temirlan <ib.temirlan@gmail.com>
2019-10-22 21:47:01 +0600

    5-quest, 9-task

---
+++ toupper.go
@@ -0,0 +1,11 @@
+package piscine
+
+func ToUpper(s string) string {
+	runeArray := []rune(s)
+	for i := range runeArray {
+		if runeArray[i] >= 'a' && runeArray[i] <= 'z' {
+			runeArray[i] = runeArray[i]-32
+		}
+	}
+	return string(runeArray)
+}
