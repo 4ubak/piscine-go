af79d93d86123a75a7ecc9bbb49115267f695ff4
Temirlan <ib.temirlan@gmail.com>
2019-10-22 21:30:58 +0600

    5-quest, 7-task

---
+++ index.go
@@ -0,0 +1,34 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	toFindCount := arrayCount(toFind)
+	secondIndex := 0
+	for i1, i2 := range s {
+		for _, j := range toFind {
+			if i2 == j {
+				if toFindCount == 1 {
+					return i1
+				}
+				for a := 0; a < toFindCount; a++ {
+					if s[i1+a] == toFind[a] {
+						secondIndex++
+					} else {
+						return -1
+					}
+				}
+				if secondIndex == toFindCount {
+					return i1
+				}
+			}
+		}
+	}
+	return -1
+}
+
+func arrayCount(s string) int {
+	count := 0
+	for i := range s {
+		count = i + 1
+	}
+	return count
+}
