7216cbf29a82db1ea708161a7a1f9d2b96e714d6
Temirlan <ib.temirlan@gmail.com>
2019-10-22 20:40:11 +0600

    5-quest, 2-task

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -1,36 +1,42 @@
-package piscine
+package main
 
 import "github.com/01-edu/z01"
 
+func main() {
+	PrintNbrInOrder(32132)
+}
+
 func PrintNbrInOrder(n int) {
 	if n == 0 {
-		z01.PrintRune('0')
+		fmt.Print(0)
 		return
 	}
 	if n > 0 {
 		var array []int
-		minValue := 0
 		eachValue := 0
-		result := 0
+
 		arrayCount := 0
+		var minValue int
 		for n != 0 {
 			eachValue = n % 10
 			n /= 10
-			if eachValue < minValue {
-				minValue = eachValue
-				array = append([]int{minValue}, array...)
-			} else {
-				minValue = eachValue
-				array = append(array, minValue)
-			}
+			array = append(array, eachValue)
 		}
-		for _, count := range array {
+
+		for count := range array {
 			arrayCount = count + 1
 		}
 		for i := 0; i < arrayCount-1; i++ {
-			result *= 10
-			result = result + array[i]
+			for j := 0; j < arrayCount-i-1; j++ {
+				if array[j] > array[j+1] {
+					minValue = array[j]
+					array[j] = array[j+1]
+					array[j+1] = minValue
+				}
+			}
 		}
-		z01.PrintRune(rune(result + 48))
+		for i := 0; i < arrayCount; i++ {
+			z01.PrintRune(rune(array[i] + 48))
+		}
 	}
 }
