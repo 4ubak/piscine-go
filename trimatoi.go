da72e9b790ad1b2f712eb28aab0093cabd0bbbf8
Temirlan <ib.temirlan@gmail.com>
2019-10-22 20:44:45 +0600

    5-quest, 3-task

---
+++ trimatoi.go
@@ -0,0 +1,41 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func TrimAtoi(s string) int {
+	var array []int
+	result := 0
+	minusIndex := 0
+	firstDigitIndex := 0
+	index := 0
+	for _, rune := range s {
+		if rune == '-' {
+			minusIndex = index
+		}
+		if isDigit(rune) {
+			if firstDigitIndex == 0 {
+				firstDigitIndex = index
+			}
+			array = append(array, int(rune-'0'))
+		}
+		index++
+	}
+
+	for i := 0; i < len(array); i++ {
+		result = result * 10 + array[i]
+	}
+	
+	if minusIndex < firstDigitIndex && minusIndex != 0 {
+		result = result * -1
+	}
+	return result
+}
+
+func isDigit(digit rune) bool {
+	for a := '0'; a <= '9'; a++ {
+		if digit == a {
+			return true
+		}
+	}
+	return false
+}
