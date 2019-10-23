a7bff43d4d8288f0ab5b11c74f06c5e6d25ac713
Temirlan <ib.temirlan@gmail.com>
2019-10-23 19:42:31 +0600

    6-quest, 3-task

--- revparams/main.go
+++ revparams/main.go
@@ -4,8 +4,6 @@ import (
 	"github.com/01-edu/z01"
 
 	"os"
-
-	"fmt"
 )
 
 func main() {
@@ -15,7 +13,6 @@ func main() {
 		count = s + 1
 	}
 	for j := count - 1; j >= 1; j-- {
-		fmt.Print(arg[j])
 		z01.PrintRune('\n')
 	}
 }
