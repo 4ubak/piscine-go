2e94f8ced99dadfa00941c9450dda4ed3066bd2b
Temirlan <ib.temirlan@gmail.com>
2019-10-24 20:33:37 +0600

    6-quest, 4-task

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,7 +1,32 @@
 package piscine
 
 func SplitWhiteSpaces(str string) []string {
-	a := make([]string, 1)
-	a = append(a, str)
-	return a
+	count := 0
+	hasSpace := false
+	for c := range str {
+
+		if hasSpace && c != 0 && (str[c-1] == '\n' || str[c-1] == '\t' || str[c-1] == ' ') && str[c] != '\n' && str[c] != '\t' && str[c] != ' ' {
+			count++
+		}
+		if str[c] != '\n' && str[c] != '\t' && str[c] != ' ' {
+			hasSpace = true
+		}
+	}
+	count++
+
+	i := 0
+	array := make([]string, count)
+	hasSpace2 := true
+	for _, c := range str {
+		if c == '\n' || c == '\t' || c == ' ' {
+			if !hasSpace2 {
+				i++
+			}
+			hasSpace2 = true
+			continue
+		}
+		array[i] = array[i] + string(c)
+		hasSpace2 = false
+	}
+	return array
 }
