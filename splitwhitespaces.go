710260d6db972bad382650cd4409c6cb21773905
4ubak <tima.75.com@gmail.com>
2019-10-25 11:20:55 +0600

    rewrited code

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,32 +1,31 @@
 package piscine
 
 func SplitWhiteSpaces(str string) []string {
-	count := 0
-	hasSpace := false
+	var arrayString []string
+	countWords := 1
+	lenStr := 0
+	result := ""
 	for c := range str {
-
-		if hasSpace && c != 0 && (str[c-1] == '\n' || str[c-1] == '\t' || str[c-1] == ' ') && str[c] != '\n' && str[c] != '\t' && str[c] != ' ' {
-			count++
+		if isWhiteSpace(str[c]) {
+			countWords++
 		}
-		if str[c] != '\n' && str[c] != '\t' && str[c] != ' ' {
-			hasSpace = true
-		}
+		lenStr++
 	}
-	count++
-
+	arrayString = make([]string, countWords)
 	i := 0
-	array := make([]string, count)
-	hasSpace2 := true
-	for _, c := range str {
-		if c == '\n' || c == '\t' || c == ' ' {
-			if !hasSpace2 {
-				i++
-			}
-			hasSpace2 = true
-			continue
+	for j, c := range str {
+		if j+1 == lenStr {
+			arrayString[i] = result + string(str[j])
 		}
-		array[i] = array[i] + string(c)
-		hasSpace2 = false
+		if isWhiteSpace(str[j]) {
+			if i <= countWords {
+				arrayString[i] = result
+				i++
+				result = ""
+			}
+		} else {
+			result += string(c)
+		}
 	}
-	return array
+	return arrayString
 }
